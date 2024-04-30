c5545c845156162c62a8a18e571050798e0530ae
Heba-Webdev23 <jtvonline@yahoo.com>
2024-04-30 20:08:47 +0300

    List of all jobs in the clinic

---
+++ app/utils/ListOfJobs.ts
@@ -0,0 +1,8 @@
+export const jobsList = [
+  { id: 1, label: "Manager", value: "Manager", type: "Staff" },
+  { id: 2, label: "HR", value: "HR", type: "Staff" },
+  { id: 3, label: "Receptionist", value: "Receptionist", type: "Staff" },
+  { id: 4, label: "Veterinarian", value: "Veterinarian", type: "Vet" },
+  { id: 5, label: "Asistant", value: "Asistant", type: "Vet" },
+  { id: 6, label: "Technician", value: "Technician", type: "Vet" },
+];
